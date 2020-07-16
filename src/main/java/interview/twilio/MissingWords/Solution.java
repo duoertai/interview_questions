d46549a12341c741910e4a34d2ce1d53bd8eeeea
ertaiduo <duoertai@gmail.com>
2020-07-15 19:28:41 -0700

    add twilio OA question

---
+++ src/main/java/interview/twilio/MissingWords/Solution.java
@@ -0,0 +1,37 @@
+package interview.twilio.MissingWords;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static List<String> findMissingWord(String t, String s) {
+        String[] tWords = t.trim().split("\\s+");
+        String[] sWords = s.trim().split("\\s+");
+
+        int tIndex = 0;
+        List<String> res = new ArrayList<>();
+
+        for(int i = 0; i < sWords.length; i++) {
+            if (tIndex == tWords.length) {
+                res.add(sWords[i]);
+            }
+            else if (sWords[i].equals(tWords[tIndex])) {
+                tIndex++;
+            } else {
+                res.add(sWords[i]);
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        List<String> res = Solution.findMissingWord(
+                "am hackerrank to improve",
+                "I am using hackerrank to improve programming"
+        );
+
+        for (String s: res)
+            System.out.println(s);
+    }
+}
