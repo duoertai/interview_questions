46603fdaae930003c9c7ee034419a702e37548a9
ertaiduo <duoertai@gmail.com>
2020-07-20 23:41:13 -0700

    add databricks question

---
+++ src/main/java/interview/databricks/FirstMissingPositive/Solution.java
@@ -0,0 +1,30 @@
+package interview.databricks.FirstMissingPositive;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 1;
+
+        for(int i = 0; i< nums.length; i++) {
+            if(nums[i] == i + 1){
+                continue;
+            }
+
+            while(nums[i] - 1 < nums.length && nums[i] - 1 >= 0 && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++)
+            if(i != nums[i] - 1)
+                return i + 1;
+
+        return nums.length + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
