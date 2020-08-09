4702bcec6d712e07b674a5c302ec920f52105925
ertaiduo <duoertai@gmail.com>
2020-08-08 20:57:43 -0700

    add pinterest question

---
+++ src/main/java/interview/pinterest/onsite/SearchInRotatedSortedArray/Solution.java
@@ -0,0 +1,47 @@
+package interview.pinterest.onsite.SearchInRotatedSortedArray;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0)
+            return -1;
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left < right - 1) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[left] < nums[mid]) {
+                if(target > nums[mid]) {
+                    left = mid;
+                } else {
+                    if(target >= nums[left]) {
+                        right = mid;
+                    } else {
+                        left = mid;
+                    }
+                }
+            } else {
+                if(target < nums[mid]) {
+                    right = mid;
+                } else {
+                    if(target <= nums[right]) {
+                        left = mid;
+                    } else {
+                        right = mid;
+                    }
+                }
+            }
+        }
+
+        if(nums[left] == target)
+            return left;
+        if(nums[right] == target)
+            return right;
+
+        return -1;
+    }
+}
